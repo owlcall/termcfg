b286ff145c7f578173ae0d1baf42eb2f5f837c2b
owl <owl@tree>
2018-05-05 16:04:52 -0700

    updated configure script

--- vim/configure.sh
+++ vim/configure.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
+ROOT=$( cd "$( dirname "$0" )" && pwd )
+
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 mkdir -p ~/.vim/colors
 cp theme.vim ~/.vim/colors/
+vim 
+vim +PluginInstall +qall
 cp airtheme.vim ~/.vim/bundle/vim-airline-themes/autoload/airline/themes/airtheme.vim
