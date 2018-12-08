62a443c56db3244c8b29cb4a50b5ed8f23c2ec71
owl <owl@tree>
2018-12-08 00:04:31 -0800

    added battery display for tmux

--- tmux/configure.sh
+++ tmux/configure.sh
@@ -2,5 +2,8 @@
 
 ROOT=$( cd "$( dirname "$0" )" && pwd )
 
+git clone https://github.com/tmux-plugins/tmux-battery $ROOT/tmux-battery
+
 echo "source $ROOT/.tmux.conf" >> ~/.tmux.conf
 echo "source-file $ROOT/cyan.tmuxtheme" >> ~/.tmux.conf
+echo "run-shell $ROOT/tmux-battery/battery.tmux" >> ~/.tmux.conf
